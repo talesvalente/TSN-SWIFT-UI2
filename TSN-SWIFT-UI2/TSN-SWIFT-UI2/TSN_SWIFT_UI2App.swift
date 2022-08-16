7a44954bb53456ac7203155f9c54706302ced299
Tales Valente <talesvalente@gmail.com>
2022-08-16 11:02:25 -0300

    CREATE CONTENT
    FIRST UPDATE

---
+++ TSN-SWIFT-UI2/TSN-SWIFT-UI2/TSN_SWIFT_UI2App.swift
@@ -0,0 +1,17 @@
+//
+//  TSN_SWIFT_UI2App.swift
+//  TSN-SWIFT-UI2
+//
+//  Created by Tales Valente on 16/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TSN_SWIFT_UI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TSN-SWIFT-UI2/TSN-SWIFT-UI2UITests/TSN_SWIFT_UI2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TSN_SWIFT_UI2UITestsLaunchTests.swift
+//  TSN-SWIFT-UI2UITests
+//
+//  Created by Tales Valente on 16/08/22.
+//
+
+import XCTest
+
+class TSN_SWIFT_UI2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
